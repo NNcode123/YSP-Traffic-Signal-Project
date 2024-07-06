593832a017ec2ac62d1f0cf9320e3f36528a1352
NNcode123 <nitrotypego@gmail.com>
2024-07-05 21:20:54 -0700

    Initialize Nerual Network
    Initialize NN

---
+++ Model_2.py
@@ -0,0 +1,230 @@
+import torch
+import torchvision
+from torchvision import transforms
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn as nn
+from torchsummary import summary
+import time
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+
+torch.cuda.empty_cache()
+
+data_transforms = transforms.Compose([
+    transforms.Resize([112, 112]),
+    transforms.ToTensor()
+    ])
+
+
+BATCH_SIZE = 256
+learning_rate = 0.001
+EPOCHS = 15
+numClasses = 43
+
+# Define path of training data
+
+train_data_path = "../GTSRB/Train"
+train_data = torchvision.datasets.ImageFolder(root = train_data_path, transform = data_transforms)
+
+# Divide data into training and validation (0.8 and 0.2)
+ratio = 0.8
+n_train_examples = int(len(train_data) * ratio)
+n_val_examples = len(train_data) - n_train_examples
+
+train_data, val_data = data.random_split(train_data, [n_train_examples, n_val_examples])
+
+train_loader = data.DataLoader(train_data, shuffle=True, batch_size = BATCH_SIZE)
+val_loader = data.DataLoader(val_data, shuffle=True, batch_size = BATCH_SIZE)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+from class_alexnetTS import AlexnetTS
+model = AlexnetTS(numClasses)
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss()
+
+if torch.cuda.is_available():
+    model = model.cuda()
+    criterion = criterion.cuda()
+
+
+def calculate_accuracy(y_pred, y):
+    top_pred = y_pred.argmax(1, keepdim = True)
+    correct = top_pred.eq(y.view_as(top_pred)).sum()
+    acc = correct.float() / y.shape[0]
+    return acc
+
+
+def evaluate(model, loader, opt, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    # Evaluate the model
+    model.eval()
+    
+    with torch.no_grad():
+        for (images, labels) in loader:
+            images = images.cuda()
+            labels = labels.cuda()
+            
+            # Run predictions
+            output, _ = model(images)
+            loss = criterion(output, labels)
+            
+            # Calculate accuracy
+            acc = calculate_accuracy(output, labels)
+            
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+    
+    return epoch_loss / len(loader), epoch_acc / len(loader)
+
+
+def train(model, loader, opt, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    # Train the model
+    model.train()
+    
+    for (images, labels) in loader:
+        images = images.cuda()
+        labels = labels.cuda()
+        
+        # Training pass
+        opt.zero_grad()
+        
+        output, _ = model(images)
+        loss = criterion(output, labels)
+        
+        # Backpropagation
+        loss.backward()
+        
+        # Calculate accuracy
+        acc = calculate_accuracy(output, labels)
+        
+        # Optimizing weights
+        opt.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(loader), epoch_acc / len(loader)
+
+train_loss_list = [0]*EPOCHS
+train_acc_list = [0]*EPOCHS
+val_loss_list = [0]*EPOCHS
+val_acc_list = [0]*EPOCHS
+
+for epoch in range(EPOCHS):
+    print("Epoch-%d: " % (epoch))
+
+    train_start_time = time.monotonic()
+    train_loss, train_acc = train(model, train_loader, optimizer, criterion)
+    train_end_time = time.monotonic()
+
+    val_start_time = time.monotonic()
+    val_loss, val_acc = evaluate(model, val_loader, optimizer, criterion)
+    val_end_time = time.monotonic()
+    
+    train_loss_list[epoch] = train_loss
+    train_acc_list[epoch] = train_acc
+    val_loss_list[epoch] = val_loss
+    val_acc_list[epoch] = val_acc
+    
+    print("Training: Loss = %.4f, Accuracy = %.4f, Time = %.2f seconds" % (train_loss, train_acc, train_end_time - train_start_time))
+    print("Validation: Loss = %.4f, Accuracy = %.4f, Time = %.2f seconds" % (val_loss, val_acc, val_end_time - val_start_time))
+    print("")
+
+import torch
+import torchvision
+from torchvision import transforms
+import torch.utils.data as data
+import time
+import numpy as np
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
+
+
+test_transforms = transforms.Compose([
+    transforms.Resize([112, 112]),
+    transforms.ToTensor()
+    ])
+
+test_data_path = "../GTSRB/Test"
+test_data = torchvision.datasets.ImageFolder(root = test_data_path, transform = test_transforms)
+test_loader = data.DataLoader(test_data, batch_size=1, shuffle=False)
+
+
+numClasses = 43
+
+# Load the saved model
+
+from class_alexnetTS import AlexnetTS
+MODEL_PATH = "../Model/pytorch_classification_alexnetTS.pth"
+model = AlexnetTS(numClasses)
+model.load_state_dict(torch.load(MODEL_PATH))
+model = model.cuda()
+
+y_pred_list = []
+corr_classified = 0
+
+numClasses = 43
+
+num = range(numClasses)
+labels = []
+for i in num:
+    labels.append(str(i))
+labels = sorted(labels)
+for i in num:
+    labels[i] = int(labels[i])
+
+df = pd.read_csv("../GTSRB/Test.csv")
+numExamples = len(df)
+labels_list = list(df.ClassId)
+
+
+with torch.no_grad():
+    model.eval()
+
+    i = 0
+
+    for image, _ in test_loader:
+        image = image.cuda()
+
+        y_test_pred = model(image)
+
+        y_pred_softmax = torch.log_softmax(y_test_pred[0], dim=1)
+        _, y_pred_tags = torch.max(y_pred_softmax, dim=1)
+        y_pred_tags = y_pred_tags.cpu().numpy()
+        
+        y_pred = y_pred_tags[0]
+        y_pred = labels[y_pred]
+        
+        y_pred_list.append(y_pred)
+
+        if labels_list[i] == y_pred:
+            corr_classified += 1
+
+        i += 1
+
+print("Number of correctly classified images = %d" % corr_classified)
+print("Number of incorrectly classified images = %d" % (numExamples - corr_classified))
+print("Final accuracy = %f" % (corr_classified / numExamples))
